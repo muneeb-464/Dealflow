2780af2afd73e0f96e59931c8df200448d17804f
Munib sajjad <464muneeb@gmail.com>
2026-08-14 00:03:10 +0500

    Update useIsAgency.ts

--- src/hooks/useIsAgency.ts
+++ src/hooks/useIsAgency.ts
@@ -1,6 +1,6 @@
 ﻿import { useUser } from "@clerk/nextjs";
 
-export function useIsAgency(): boolean {
+export function useIsAgency(): boolean { 
   const { user } = useUser();
   return user?.unsafeMetadata?.accountType === "business";
 }
